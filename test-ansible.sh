2f24d0ed4c1ae45c5cc27dadda4fd397e88002a2
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-11-04 18:33:17 +0100

    code review

--- test-ansible.sh
+++ test-ansible.sh
@@ -5,14 +5,14 @@ CONJUR_MASTER_PORT=3000
 export CONJUR_APPLIANCE_URL=https://$CONJUR_MASTER_HOSTNAME:$CONJUR_MASTER_PORT
 export CONJUR_AUTHN_API_KEY=$ANSIBLE_KEY
 export CONJUR_AUTHN_LOGIN=host/cd/ansible/controller
-export CONJUR_CERT_FILE=conjur.pem
+export CONJUR_CERT_FILE=/tmp/conjur.pem
 export CONJUR_ACCOUNT=devsecops
 
-ansible-galaxy collection install cyberark.conjur
+#ansible-galaxy collection install cyberark.conjur
 
 openssl s_client -connect "$CONJUR_MASTER_HOSTNAME":"$CONJUR_MASTER_PORT" \
   -showcerts </dev/null 2> /dev/null | \
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
-  > "$CONJUR_CERT_FILE"
+  > "/tmp/$CONJUR_CERT_FILE"
 
 ansible-playbook -i inventory playbook.yml
