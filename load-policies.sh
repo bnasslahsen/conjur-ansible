fc7b25d999313a2da16a67f624474dabfef94adf
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-01-16 09:16:13 +0100

    code review

--- load-policies.sh
+++ load-policies.sh
@@ -9,6 +9,6 @@ openssl s_client -connect "$CONJUR_MASTER_HOSTNAME":"$CONJUR_MASTER_PORT" \
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
   > "$CONJUR_CERT_FILE"
 
-conjur policy update -b root -f ansible-policies.yml
+conjur policy load -b root -f ansible-policies.yml > ansibe-host.key
 conjur variable set -i cd/ansible/secrets/test-password -v "toto"
 conjur variable set -i cd/ansible/secrets/ansible-key -v "toto"
